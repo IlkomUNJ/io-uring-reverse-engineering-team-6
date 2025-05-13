47584d5ebea3b316374295ca6d58166168d5b04a
HansV125 <147579603+HansV125@users.noreply.github.com>
2025-05-13 22:36:01 +0700

    Update register.h

--- io_uring/register.h
+++ io_uring/register.h
@@ -2,8 +2,14 @@
 #ifndef IORING_REGISTER_H
 #define IORING_REGISTER_H
 
+//Menghapus pendaftaran eventfd dari konteks io_uring.
 int io_eventfd_unregister(struct io_ring_ctx *ctx);
+
+//Menghapus pendaftaran "personality" dalam io_uring.
+//Personality adalah fitur yang memungkinkan pengguna untuk mengisolasi operasi IO tertentu dengan ID unik.
 int io_unregister_personality(struct io_ring_ctx *ctx, unsigned id);
+
+//berfungsi untuk mendapatkan objek file dari file descriptor (fd) yang terdaftar di io_uring.
 struct file *io_uring_register_get_file(unsigned int fd, bool registered);
 
 #endif
