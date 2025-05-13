9b2b4396cf16cb6902d0938dc137a974f962f48e
HansV125 <147579603+HansV125@users.noreply.github.com>
2025-05-13 21:54:29 +0700

    Update nop.h

--- io_uring/nop.h
+++ io_uring/nop.h
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 
+//meyiapkan request nop (no operations)
 int io_nop_prep(struct io_kiocb *req, const struct io_uring_sqe *sqe);
+//mengeksekusi suatu request nop (no operation)
 int io_nop(struct io_kiocb *req, unsigned int issue_flags);
