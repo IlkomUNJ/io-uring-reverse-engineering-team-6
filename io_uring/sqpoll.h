7dd4547d315892593b0ed4b1d27395300a66789e
raja <leanore963@gmail.com>
2025-05-13 15:19:58 +0700

    update tsk4

--- io_uring/sqpoll.h
+++ io_uring/sqpoll.h
@@ -1,31 +1,54 @@
 // SPDX-License-Identifier: GPL-2.0
 
+// Struktur untuk data terkait dengan thread pada submission queue (SQ)
 struct io_sq_data {
-	refcount_t		refs;
-	atomic_t		park_pending;
-	struct mutex		lock;
+    refcount_t refs;
+    atomic_t park_pending;
+    struct mutex lock;
 
-	/* ctx's that are using this sqd */
-	struct list_head	ctx_list;
+    struct list_head ctx_list;
 
-	struct task_struct	*thread;
-	struct wait_queue_head	wait;
+    struct task_struct *thread;
+    struct wait_queue_head wait;
 
-	unsigned		sq_thread_idle;
-	int			sq_cpu;
-	pid_t			task_pid;
-	pid_t			task_tgid;
+    unsigned sq_thread_idle;
+    int sq_cpu;
+    pid_t task_pid;
+    pid_t task_tgid;
 
-	u64			work_time;
-	unsigned long		state;
-	struct completion	exited;
+    u64 work_time;
+    unsigned long state;
+    struct completion exited;
 };
 
-int io_sq_offload_create(struct io_ring_ctx *ctx, struct io_uring_params *p);
-void io_sq_thread_finish(struct io_ring_ctx *ctx);
-void io_sq_thread_stop(struct io_sq_data *sqd);
-void io_sq_thread_park(struct io_sq_data *sqd);
-void io_sq_thread_unpark(struct io_sq_data *sqd);
-void io_put_sq_data(struct io_sq_data *sqd);
-void io_sqpoll_wait_sq(struct io_ring_ctx *ctx);
-int io_sqpoll_wq_cpu_affinity(struct io_ring_ctx *ctx, cpumask_var_t mask);
+// Fungsi untuk membuat offload submission queue untuk io_uring
+int io_sq_offload_create(struct io_ring_ctx *ctx, struct io_uring_params *p) {
+}
+
+// Fungsi untuk menyelesaikan eksekusi thread submission queue
+void io_sq_thread_finish(struct io_ring_ctx *ctx) {
+}
+
+// Fungsi untuk menghentikan thread submission queue
+void io_sq_thread_stop(struct io_sq_data *sqd) {
+}
+
+// Fungsi untuk memarkir thread submission queue
+void io_sq_thread_park(struct io_sq_data *sqd) {
+}
+
+// Fungsi untuk membangunkan thread submission queue yang diparkir
+void io_sq_thread_unpark(struct io_sq_data *sqd) {
+}
+
+// Fungsi untuk mengurangi referensi terhadap data submission queue dan membersihkan jika perlu
+void io_put_sq_data(struct io_sq_data *sqd) {
+}
+
+// Fungsi untuk menunggu antrian pekerjaan pada submission queue
+void io_sqpoll_wait_sq(struct io_ring_ctx *ctx) {
+}
+
+// Fungsi untuk menetapkan afinitas CPU untuk polling submission queue
+int io_sqpoll_wq_cpu_affinity(struct io_ring_ctx *ctx, cpumask_var_t mask) {
+}
